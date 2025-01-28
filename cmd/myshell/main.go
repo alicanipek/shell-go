edc9312cc201c457818b60e178048f5838238008
Ali Can İPEK <alican.ipek@outlook.com>
2025-01-28 23:21:51 +0300

    repl

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,14 +10,16 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		// Uncomment this block to pass the first stage
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		os.Exit(1)
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			os.Exit(1)
+		}
+
+		fmt.Fprint(os.Stdout, command[:len(command)-1]+": command not found\n")
 	}
-
-	fmt.Fprint(os.Stdout, command[:len(command)-1]+": command not found\n")
 }
