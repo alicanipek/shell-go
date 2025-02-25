2d482383bf6fefe93bffdac0cd7e73b707f936f3
Ali Can İPEK <alican.ipek@outlook.com>
2025-02-25 22:50:56 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -22,11 +22,12 @@ func main() {
 			os.Exit(1)
 		}
 		command = strings.Trim(command, "\n")
-		parts := parseInput(command)
+
+		parts, file := parseInput(command)
 
 		com := parts[0]
 		args := parts[1:]
-		print(args)
+
 		switch com {
 		case "exit":
 			exitCode, err := strconv.Atoi(args[0])
@@ -35,10 +36,10 @@ func main() {
 			}
 			os.Exit(exitCode)
 		case "echo":
-			fmt.Fprintln(os.Stdout, strings.Join(args, " "))
+			fmt.Fprintln(file, strings.Join(args, " "))
 		case "type":
 			if slices.Contains(builtins, args[0]) {
-				fmt.Fprintln(os.Stdout, args[0]+" is a shell builtin")
+				fmt.Fprintln(file, args[0]+" is a shell builtin")
 			} else {
 				inPath := isExecutableInPath(args[0])
 				if inPath != "" {
@@ -64,33 +65,65 @@ func main() {
 			if err != nil {
 				fmt.Println("cd: " + args[0] + ": No such file or directory")
 			}
+		case "cat":
+			execute(com, args, file)
 		default:
-			err := execute(com, args)
+			err := execute(com, args, file)
 			if err != nil {
 				fmt.Fprintln(os.Stdout, command+": command not found")
 			}
 		}
-
+		if file != nil && file != os.Stdout {
+			file.Close()
+		}
+		file = os.Stdout
 	}
 }
 
-func execute(executable string, args []string) error {
-
+func execute(executable string, args []string, file *os.File) error {
 	cmd := exec.Command(executable, args...)
 	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
+	cmd.Stdout = file
 	er := cmd.Run()
+
 	return er
 }
 
-func parseInput(input string) []string {
+func parseInput(input string) ([]string, *os.File) {
 	var args []string
+	var file *os.File
+	command := input
 	var token strings.Builder
 	inSingleQuote := false
 	inDoubleQuote := false
 	escapeNext := false
+	var splitInput []string
+	if strings.Contains(input, "1>") {
+		splitInput = strings.Split(input, "1>")
+	} else if strings.Contains(input, ">") {
+		splitInput = strings.Split(input, ">")
+	}
+	if len(splitInput) > 1 {
+		// abs, _ := filepath.Abs(strings.TrimSpace(splitInput[1]))
+		// parentDir := filepath.Dir(abs)
+		// err := os.MkdirAll(parentDir, 0755) // 0755 is the permission mode (rwxr-xr-x)
+		// if err != nil {
+		// 	fmt.Println("Error creating directories:", err)
+		// 	os.Exit(1)
+		// }
 
-	for i := 0; i < len(input); i++ {
+		// // Create the file
+		// f, err := os.Create(abs)
+		// if err != nil {
+		// 	fmt.Println("Error creating files:", err)
+		// 	os.Exit(1)
+		// }
+		file, _ = os.OpenFile(strings.TrimSpace(splitInput[1]), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+		command = strings.TrimSpace(splitInput[0])
+	} else {
+		file = os.Stdout
+	}
+	for i := range len(command) {
 		char := input[i]
 
 		switch {
@@ -132,7 +165,6 @@ func parseInput(input string) []string {
 				args = append(args, token.String())
 				token.Reset()
 			}
-
 		default:
 			// Add character to current token
 			token.WriteByte(char)
@@ -144,7 +176,7 @@ func parseInput(input string) []string {
 		args = append(args, token.String())
 	}
 
-	return args
+	return args, file
 }
 
 func isExecutableInPath(executable string) string {
