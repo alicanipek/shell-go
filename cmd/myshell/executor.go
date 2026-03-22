c07179306cfa5258d761e76720233052c67abc18
alicanipek <alican.ipek@outlook.com>
2026-03-22 14:15:14 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/executor.go
+++ cmd/myshell/executor.go
@@ -12,97 +12,126 @@ import (
 	"sync"
 )
 
-var builtins = []string{"type", "exit", "echo", "pwd", "cd"}
+var builtins = []string{"type", "exit", "echo", "pwd", "cd", "history"}
 
-func execute(commands []Command) {
+// ExitError is returned when the shell's exit builtin is invoked.
+type ExitError struct{ Code int }
+
+func (e *ExitError) Error() string { return fmt.Sprintf("exit: %d", e.Code) }
+
+var builtinHandlers = map[string]func(Command) error{
+	"exit":    builtinExit,
+	"echo":    builtinEcho,
+	"type":    builtinType,
+	"pwd":     builtinPwd,
+	"cd":      builtinCd,
+	"history": builtinHistory,
+}
+
+func builtinHistory(cmd Command) error {
+	return nil
+}
+
+func builtinExit(cmd Command) error {
+	code := 0
+	if len(cmd.Args) > 0 {
+		var err error
+		if code, err = strconv.Atoi(cmd.Args[0]); err != nil {
+			code = 1
+		}
+	}
+	return &ExitError{code}
+}
+
+func builtinEcho(cmd Command) error {
+	fmt.Fprintln(cmd.Stdout, strings.Join(cmd.Args, " "))
+	return nil
+}
+
+func builtinType(cmd Command) error {
+	if len(cmd.Args) == 0 {
+		return nil
+	}
+	if slices.Contains(builtins, cmd.Args[0]) {
+		fmt.Fprintln(cmd.Stdout, cmd.Args[0]+" is a shell builtin")
+	} else if desc := isExecutableInPath(cmd.Args[0]); desc != "" {
+		fmt.Fprintln(cmd.Stdout, desc)
+	} else {
+		fmt.Fprintln(cmd.Stderr, cmd.Args[0]+": not found")
+	}
+	return nil
+}
+
+func builtinPwd(cmd Command) error {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintln(cmd.Stderr, "pwd: error getting current directory")
+		return nil
+	}
+	fmt.Fprintln(cmd.Stdout, dir)
+	return nil
+}
+
+func builtinCd(cmd Command) error {
+	if len(cmd.Args) == 0 {
+		return nil
+	}
+	p := cmd.Args[0]
+	if p == "~" {
+		p, _ = os.UserHomeDir()
+	}
+	if err := os.Chdir(p); err != nil {
+		fmt.Fprintf(cmd.Stderr, "cd: %s: No such file or directory\n", cmd.Args[0])
+	}
+	return nil
+}
+
+func execute(commands []Command) error {
 	if len(commands) == 1 {
 		cmd := commands[0]
 		defer closeCommandStreams(cmd)
-
-		// Handle exit specially - only exit when it's a standalone command
-		if cmd.Name == "exit" {
-			code := 0
-			if len(cmd.Args) > 0 {
-				var err error
-				if code, err = strconv.Atoi(cmd.Args[0]); err != nil {
-					code = 1
-				}
-			}
-			os.Exit(code)
-		}
-
-		executeCmd(cmd)
-		return
+		return executeCmd(cmd)
 	}
-
-	executePipeline(commands)
+	return executePipeline(commands)
 }
 
-func executePipeline(commands []Command) {
+func executePipeline(commands []Command) error {
 	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var firstErr error
 
 	for i, command := range commands {
 		wg.Add(1)
 		go func(idx int, c Command) {
 			defer wg.Done()
 			defer closeCommandStreams(c)
-			executeCmd(c)
+			if err := executeCmd(c); err != nil {
+				mu.Lock()
+				if firstErr == nil {
+					firstErr = err
+				}
+				mu.Unlock()
+			}
 		}(i, command)
 	}
 	wg.Wait()
+	return firstErr
 }
 
-func executeCmd(cmd Command) {
-	switch cmd.Name {
-	case "exit":
-		// In a pipeline, exit is a no-op (it's handled in execute() for standalone commands)
-		return
-
-	case "echo":
-		fmt.Fprintln(cmd.Stdout, strings.Join(cmd.Args, " "))
-
-	case "type":
-		if len(cmd.Args) == 0 {
-			return
-		}
-		if slices.Contains(builtins, cmd.Args[0]) {
-			fmt.Fprintln(cmd.Stdout, cmd.Args[0]+" is a shell builtin")
-		} else if desc := isExecutableInPath(cmd.Args[0]); desc != "" {
-			fmt.Fprintln(cmd.Stdout, desc)
-		} else {
-			fmt.Fprintln(cmd.Stderr, cmd.Args[0]+": not found")
-		}
-
-	case "pwd":
-		dir, err := os.Getwd()
-		if err != nil {
-			fmt.Fprintln(cmd.Stderr, "pwd: error getting current directory")
-			return
-		}
-		fmt.Fprintln(cmd.Stdout, dir)
-
-	case "cd":
-		if len(cmd.Args) == 0 {
-			return
-		}
-		p := cmd.Args[0]
-		if p == "~" {
-			p, _ = os.UserHomeDir()
-		}
-		if err := os.Chdir(p); err != nil {
-			fmt.Fprintf(cmd.Stderr, "cd: %s: No such file or directory\n", cmd.Args[0])
-		}
-
-	default:
-		c := exec.Command(cmd.Name, cmd.Args...)
-		c.Stdin = cmd.Stdin
-		c.Stdout = cmd.Stdout
-		c.Stderr = cmd.Stderr
-
-		if err := c.Run(); err != nil && isNotFound(err) {
-			fmt.Fprintln(cmd.Stderr, cmd.Name+": command not found")
-		}
+func executeCmd(cmd Command) error {
+	if handler, ok := builtinHandlers[cmd.Name]; ok {
+		return handler(cmd)
 	}
+
+	c := exec.Command(cmd.Name, cmd.Args...)
+	c.Stdin = cmd.Stdin
+	c.Stdout = cmd.Stdout
+	c.Stderr = cmd.Stderr
+
+	if err := c.Run(); err != nil && isNotFound(err) {
+		fmt.Fprintln(cmd.Stderr, cmd.Name+": command not found")
+	}
+	return nil
 }
 
 func isExecutableInPath(exe string) string {
--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io"
 	"os"
 	"strings"
 	"sync"
@@ -40,44 +40,11 @@ func (s *Shell) run() {
 		if len(commands) == 0 {
 			continue
 		}
-		execute(commands)
-
-		// if len(commands) == 1 {
-		// 	s.dispatch(commands[0])
-		// 	closeIfNotStd(commands[0].Stdout)
-		// 	closeIfNotStd(commands[0].Stderr)
-		// 	closeIfNotStd(commands[0].Stdin)
-		// } else {
-		// 	c1 := exec.Command(commands[0].Name, commands[0].Args...)
-		// 	c1.Stdout = commands[0].Stdout
-		// 	c1.Stderr = commands[0].Stderr
-		// 	c1.Stdin = commands[0].Stdin
-		// 	c2 := exec.Command(commands[1].Name, commands[1].Args...)
-		// 	c2.Stdout = commands[1].Stdout
-		// 	c2.Stderr = commands[1].Stderr
-		// 	c2.Stdin = commands[1].Stdin
-
-		// 	pr, pw := io.Pipe()
-		// 	c1.Stdout = pw
-		// 	c2.Stdin = pr
-		// 	c2.Stdout = os.Stdout
-
-		// 	c1.Start()
-		// 	c2.Start()
-		// 	go func() {
-		// 		defer pw.Close()
-
-		// 		c1.Wait()
-		// 	}()
-		// 	c2.Wait()
-
-		// }
+		if err := execute(commands); err != nil {
+			var exitErr *ExitError
+			if errors.As(err, &exitErr) {
+				os.Exit(exitErr.Code)
+			}
+		}
 	}
-}
-
-func closeIfNotStd(f *io.Writer) {
-	if closer, ok := (*f).(io.Closer); ok {
-		closer.Close()
-	}
-
 }
